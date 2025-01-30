d32b92046e180a5391da9760f06b92e565b5c729
nang nuyin <nangnuyin005491@gmail.com>
2025-01-30 11:58:18 +0630

    customize

--- customer/customize.php
+++ customer/customize.php
@@ -1,31 +1,86 @@
-<?php require_once('layout/header.php') ?>
-<?php require_once('layout/nav.php') ?>
-<?php require_once('storage/db.php') ?>
-<?php require_once('storage/plantcrud.php') ?>
-<?php require_once('storage/cart_crud.php')?>
+<?php require_once('./layout/header.php') ?>
+<?php require_once('./layout/nav.php') ?>
+<?php require_once('../storage/db.php') ?>
+<?php require_once('../storage/bouquet_crud.php') ?>
 <?php require_once('../storage/flower_crud.php') ?>
-</div>
-</header>
-<div class="container">
-<table>
-<div class=" gy-4">
-  <?php $i = 1;
-  $flower_list = get_all_flower($mysqli); ?>
-  <?php while ($flower = $flower_list->fetch_assoc()) { ?>
-    <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="100">
-      <div class="pricing-item">
-        <h3><?= $flower['name'] ?></h3>
-        <img src="../assets/img/<?= $flower['img'] ?>" alt="">
-        <h5><?= $flower['price'] ?><span> mmk</span></h5>
-        <div class="btn-wrap">
-          <a href="customizedetails.php?id=<?= $flower['id'] ?>" class="btn-buy">Buy Now</a>
+
+
+  <main class="main">
+
+    <section id="pricing" class="pricing section light-background">
+
+
+      <div class="container">
+        <div class="card">
+    
+
+        <div class="row gy-2">
+
+          <div class="col-lg-12 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="pricing">
+              <div class="bouquet-item m-5">
+              <div class="d-flex justify-content-between">
+              <h3>Flower List</h3>
+                <a href="add_flower.php" class="btn-buy"
+                  style="padding: 10px 15px; background-color: var(--accent-color); border-radius: 18px; color:white;">Add New Flower</a>
+              </div>
+                <div class="card m-3 border-0">
+                  <div class="card-body">
+                  
+                    <table class="table table-bordered table-striped">
+                      <thead>
+                        <tr>
+                          <th>No</th>
+                          <th>Flower Name</th>
+                          <th>Color</th>
+                          <th>Qty</th>
+                          <th>Price</th>                     
+                          <th>Image</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php $i=1; $flower_list = get_all_flower($mysqli); ?>
+                        
+                        <?php while ($flower = $flower_list->fetch_assoc()) { ?>
+
+                          <tr>
+                            <td><?= $i ?></td>
+                            <td><?= $flower['name'] ?></td>
+                            <td><?= $flower['color'] ?></td>
+                            <td><?= $flower['qty'] ?></td>
+                            <td><?= $flower['price'] ?> mmk</td>
+                            <td>
+                              <img src="" alt="">
+                            </td>
+                            <th>
+                            <a class="btn btn-sm btn-primary" href="./add_flower.php?id=<?= $flower['id'] ?>"><i class="bi bi-pen"></i></a>
+                            <button class="btn btn-sm btn-danger deleteSelect" data-val="<?= $flower['id'] ?>"
+                            data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="bi bi-trash"></i></button>
+                            </th>
+                          
+                          </tr>
+                          <?php $i++;
+                        }
+                        ?>
+                      </tbody>
+                    </table>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div><!-- End Pricing Item -->
+
+
         </div>
+
       </div>
-    </div>
-    <?php $i++;
-  } ?><!-- End Pricing Item -->
-  </table>
-</div>
+
+    </section><!-- /Pricing Section -->
 
 
-  <?php require_once('layout/footer.php') ?>
+
+  </main>
+
+ <?php require_once("../layout/footer.php") ?>
